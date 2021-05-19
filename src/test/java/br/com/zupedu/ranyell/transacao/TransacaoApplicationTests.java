08ad1efa563970916e8dbc321521e43c623f63ce
RanyellHenrique <Ranyellhenrique@gmail.com>
2021-05-19 09:05:03 -0300

    setup do projeto

---
+++ src/test/java/br/com/zupedu/ranyell/transacao/TransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupedu.ranyell.transacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
